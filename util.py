b6eadeffd91793df3829d34e4dfd0efecc02244d
Mussie Nadew <muchichi@gmail.com>
2017-04-09 09:28:30 -0400

    simple palindrom function

--- util.py
+++ util.py
@@ -3,16 +3,19 @@
 def main():
     #print('Palindroms')
     check_palindromes('Never Odd or Even')
-    check_palindromes('Madam, I''m Adam')
+    #check_palindromes('Madam, I''m Adam')
+    simple_pali('Never Odd or Even')
 
 def clean_text(text):
-    pal = text.lower()
-    punctuations = [' ',',','!','?',';','.','e']
-    for x in list(punctuations):
-        pal.replace(x,'')
+    pal = text.lower().strip()
+    #punctuations = [' ,,!,\',;,:]
+    #pal.translate(None, str.punctuation)
+    pal.replace(' ','')
+    return pal
     
 
-def check_palindromes(pal):   
+def check_palindromes(word):   
+    pal = clean_text(word)
     mid,half,rest = 0,'',''
     x = int(len(pal))
     if(x%2 > 0):
@@ -20,10 +23,20 @@ def check_palindromes(pal):
     else:        
         mid = (x//2)
           
-    half = pal[:mid]
-    rest = pal[mid:x][::-1]
-    print('First part \t\t: \'{}\' Second part\t\t: \'{}\''.format(half,rest))
+    half = pal[:mid+1]
+    rest = pal[mid:][::-1]
+    print('First part: \'{}\' Second part: \'{}\''.format(half,rest))
     print(half.replace(' ','') == rest.replace(' ',''))
+    
+def simple_pali(word):
+    pal = clean_text(word)
+    if(pal[::].replace(' ','') == pal[::-1].replace(' ','')):
+       print('We got palindrom >> {}'.format(pal[::]))
+    else:
+       print('Not at all >> {}'.format(pal[::-1].replace(' ','')))
+        
+        
+        
     
 if __name__=='__main__': main()
     
